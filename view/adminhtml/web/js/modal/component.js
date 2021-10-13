6f1ee957f31baa8c603838070b8f46b97b9e8c18
Yegor Shytikov <egorshitiov@gmail.com>
2021-10-13 15:26:34 -0700

    Magento Fork initial commit

--- view/adminhtml/web/js/modal/component.js
+++ view/adminhtml/web/js/modal/component.js
@@ -20,7 +20,16 @@ define([
                         enableLogAction: '${ $.provider }:data.enableLogAction',
                         disableLogAction: '${ $.provider }:data.disableLogAction'
                     },
-                    options: {},
+                    options: {
+                        keyEventHandlers: {
+                            /**
+                             * Prevents escape key from exiting out of modal
+                             */
+                            escapeKey: function () {
+                                return;
+                            }
+                        }
+                    },
                     notificationWindow: null
                 },
 
@@ -33,31 +42,10 @@ define([
                 },
 
                 /**
-                 * Configure ESC and TAB so user can't leave modal
-                 * without selecting an option
-                 *
-                 * @returns {Object} Chainable.
-                 */
-                initModalEvents: function () {
-                    this._super();
-                    //Don't allow ESC key to close modal
-                    this.options.keyEventHandlers.escapeKey = this.handleEscKey.bind(this);
-                    //Restrict tab action to the modal
-                    this.options.keyEventHandlers.tabKey = this.handleTabKey.bind(this);
-
-                    return this;
-                },
-
-                /**
                  * Once the modal is opened it hides the X
                  */
                 onOpened: function () {
-                    $('.modal-header button.action-close').attr('disabled', true).hide();
-
-                    this.focusableElements = $(this.rootSelector).find('a[href], button:enabled');
-                    this.firstFocusableElement = this.focusableElements[0];
-                    this.lastFocusableElement = this.focusableElements[this.focusableElements.length - 1];
-                    this.firstFocusableElement.focus();
+                    $('.modal-header button.action-close').hide();
                 },
 
                 /**
@@ -116,70 +104,11 @@ define([
                  * Allows admin usage popup to be shown first and then new release notification
                  */
                 openReleasePopup: function () {
-                    var notificationModalSelector = 'release_notification.release_notification.notification_modal_1';
+                    var notifiModal = registry.get('release_notification.release_notification.notification_modal_1');
 
                     if (analyticsPopupConfig.releaseVisible) {
-                        registry.get(notificationModalSelector).initializeContentAfterAnalytics();
+                        notifiModal.initializeContentAfterAnalytics();
                     }
-                },
-
-                /**
-                 * Handle Tab and Shift+Tab key event
-                 *
-                 * Keep the tab actions restricted to the popup modal
-                 * so the user must select an option to dismiss the modal
-                 */
-                handleTabKey: function (event) {
-                    var modal = this,
-                        KEY_TAB = 9;
-
-                    /**
-                     * Handle Shift+Tab to tab backwards
-                     */
-                    function handleBackwardTab() {
-                        if (document.activeElement === modal.firstFocusableElement ||
-                            document.activeElement === $(modal.rootSelector)[0]
-                        ) {
-                            event.preventDefault();
-                            modal.lastFocusableElement.focus();
-                        }
-                    }
-
-                    /**
-                     * Handle Tab forward
-                     */
-                    function handleForwardTab() {
-                        if (document.activeElement === modal.lastFocusableElement) {
-                            event.preventDefault();
-                            modal.firstFocusableElement.focus();
-                        }
-                    }
-
-                    switch (event.keyCode) {
-                        case KEY_TAB:
-                            if (modal.focusableElements.length === 1) {
-                                event.preventDefault();
-                                break;
-                            }
-
-                            if (event.shiftKey) {
-                                handleBackwardTab();
-                                break;
-                            }
-                            handleForwardTab();
-                            break;
-                        default:
-                            break;
-                    }
-                },
-
-                /**
-                 * Handle Esc key
-                 *
-                 * Esc key should not close modal
-                 */
-                handleEscKey: function (event) {
-                    event.preventDefault();
                 }
             }
         );
